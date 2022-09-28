4f3ef9489444c9f6ec88e4fcf5057ce903bb0b48
Marko Vukovic <marko.vuk987@gmail.com>
2022-09-28 11:55:37 +0200

    test_done

--- main.py
+++ main.py
@@ -5,22 +5,22 @@
 import gpio
 import timers
 
-global counter, curr_color
 counter = 0
 
-COL_RED   = 1
-COL_BLUE  = 2
+COL_CLEAR = 0
+COL_RED   = 2
+COL_BLUE  = 1
 COL_GREEN = 3
 
-S0 = D2
-S1 = D3
+S0 = D18
+S1 = D19
 S2 = D26
 S3 = D25
 OUT = D35
 LED = D15
 
 color = (0, 0, 0)
-curr_color = 0
+curr_color = COL_CLEAR
 
 def print_color(color):
     print(color)
@@ -46,20 +46,21 @@ def prepare_blue():
     
 def switch_col():
     print("Int")
-    # global counter, curr_color
-    curr_color += 1 
+    global counter
+    global curr_color
+    curr_color += 1
     
-    if (curr_color == COL_RED):
-        print("Red")
-        color[COL_RED] = counter
-        prepare_blue()
-    elif (curr_color == 2):
-        print("Blue")
-        color[COL_BLUE] = counter
+    if (curr_color == COL_BLUE):
+        print("Blue", counter)
+        # color[COL_BLUE] = counter
         prepare_green()
+    elif (curr_color == COL_RED):
+        print("Red", counter)
+        # color[COL_RED] = counter
+        prepare_blue()
     elif (curr_color == COL_GREEN):
-        print("green")
-        color[COL_GREEN] = counter
+        print("green", counter)
+        # color[COL_GREEN] = counter
     else:
         print_color(color)
         curr_color = 0
@@ -75,7 +76,7 @@ gpio.mode(LED, OUTPUT)  # Set mode for D10
 gpio.mode(S0, OUTPUT)   # Set mode for S0
 gpio.mode(S1, OUTPUT)   # Set mode for S1
 gpio.low(S0)
-gpio.low(S1)
+gpio.high(S1)
 
 gpio.mode(S2, OUTPUT)   # Set mode for S2
 gpio.mode(S3, OUTPUT)   # Set mode for S3
